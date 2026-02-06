2b56e0e990ac1564dbf36f4cffb388956fa75d06
byeonggubaek <oranjes@naver.com>
2026-02-06 12:11:06 +0900

    Bootsrtap 적용함

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react'
 import { greet, type ApiResponse } from 'shared'
+import 'bootstrap/dist/css/bootstrap.min.css';  // ← 여기에 추가
+import { Button } from 'react-bootstrap';       // react-bootstrap만
 
 function App() {
   const [data, setData] = useState<ApiResponse<string> | null>(null)
@@ -12,6 +14,8 @@ function App() {
 
   return (
     <div className="p-8 max-w-4xl mx-auto">
+      <Button variant="primary">첫번째</Button>
+      <Button variant="secoundary">두번째</Button>
       <h1 className="text-3xl font-bold mb-8">{greet('ESM Frontend')}</h1>
       <pre className="bg-gray-100 p-4 rounded overflow-auto">
         {JSON.stringify(data, null, 2)}
